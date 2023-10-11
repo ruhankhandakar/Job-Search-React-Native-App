a0efb92ad9743d06f24ee3e2eafefffad5b1a4a8
Ruhan Khandakar <khandakarruhan@gmail.com>
2023-10-11 15:58:43 +0600

    feat: ui completed

--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -3,12 +3,14 @@ import { View, Text, TouchableOpacity, Image } from 'react-native';
 
 import styles from './screenheader.style';
 
-const ScreenHeaderBtn = ({ iconUrl, dimension }) => {
-  const handlePress = () => {
-    console.log('Pressing');
+const ScreenHeaderBtn = ({ iconUrl, dimension, handlePress }) => {
+  const onPress = () => {
+    if (handlePress) {
+      handlePress();
+    }
   };
   return (
-    <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
+    <TouchableOpacity style={styles.btnContainer} onPress={onPress}>
       <Image
         source={iconUrl}
         resizeMode="cover"
