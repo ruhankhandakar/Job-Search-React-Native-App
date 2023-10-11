1f125e034b7311d52ab69162e831ce5239a90e36
Ruhan Khandakar <khandakarruhan@gmail.com>
2023-10-11 16:11:42 +0600

    handle search functionality

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -13,8 +13,8 @@ const NearbyJobCard = ({ item, handleCardPress }) => {
       <TouchableOpacity style={styles.logoContainer}>
         <Image
           source={{
-            uri: checkImageURL(item.employer_logo)
-              ? item.employer_logo
+            uri: checkImageURL(item?.employer_logo)
+              ? item?.employer_logo
               : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg',
           }}
           resizeMode="contain"
@@ -23,9 +23,9 @@ const NearbyJobCard = ({ item, handleCardPress }) => {
       </TouchableOpacity>
       <View style={styles.textContainer}>
         <Text style={styles.jobName} numberOfLines={1}>
-          {item.job_title}
+          {item?.job_title || ''}
         </Text>
-        <Text style={styles.jobType}>{item.job_employment_type}</Text>
+        <Text style={styles.jobType}>{item?.job_employment_type || ''}</Text>
       </View>
     </TouchableOpacity>
   );
